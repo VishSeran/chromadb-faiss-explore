8f334226c9f166f47b45cbdcd0cbf31e62daf5f7
VishSeran <vishwa.sovis@gmail.com>
2026-06-17 14:02:17 +0530

    chroma-main updated

--- chroma/main.py
+++ chroma/main.py
@@ -1,9 +1,15 @@
 import chroma
 from chroma.chroma_utils import collection_name, chroma_client, ef
+from logger import get_logger
 
+logger = get_logger("chroma-main")
 
 def main():
     try:
+        
+        if not collection_name:
+            raise ValueError("collection name is empty or none")
+         
         collection = chroma_client.create_collection(
             name=collection_name,
             metadata={"description": "A collection for storing grocery data"},
@@ -15,7 +21,12 @@ def main():
             }
         )
         
+        if not collection:
+            raise ValueError(f"{collection_name} is not found")
+        
+        logger.info(f"{collection_name} is created")
+        
         
     except Exception as e:
-        print(f"Unknown error: {e}")    
+        logger.error(f"Unknown error: {e}")    
         return str({e})
